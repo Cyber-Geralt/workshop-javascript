4060c38e24895937a3af1e8aada75cbc7a85b4e4
Alan Sek <alan-zielinski@hotmail.com>
2022-06-21 09:37:53 +0200

    Add static/ directory

--- videos/scripts/main.js
+++ videos/scripts/main.js
@@ -1,14 +1,1 @@
-const thumbnails = document.querySelectorAll('img');
-const outlet = document.querySelector('#outlet');
-
-thumbnails.forEach((thumbnail) => {
-    thumbnail.addEventListener('click',() => {
-    const currentSource = thumbnail.src;
-    const biggerPhotoSource = currentSource.replace("150/100","300/200");
-    //console.log(biggerPhotoSource);
-    //outlet.innerHTML = '<img src="'+biggerPhotoSource+'" alt=""/>';
-    if(outlet){
-        outlet.innerHTML = `<img src="${biggerPhotoSource}" alt=""/>`;
-    }
-   })
-})
+console.log("tets");
